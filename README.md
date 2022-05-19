994b9c650ea281e03b0b0b774c53384deb7277d5
willphp <24203741@qq.com>
2022-05-19 14:57:09 +0800

    v1.1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 数据集合
+#数据集合
 
 collection组件提供方便的封装来进行数据操作
 
@@ -6,11 +6,11 @@ collection组件提供方便的封装来进行数据操作
 
 ####安装组件
 
-使用 composer 命令进行安装或下载源代码使用(依赖willphp/page组件)。
+使用 composer 命令进行安装或下载源代码使用，依赖(willphp/page)。
 
     composer require willphp/collection
 
-> WillPHP 框架已经内置此组件，无需再安装。
+> WillPHP框架已经内置此组件，无需再安装。
 
 ####建立集合
 
@@ -31,9 +31,5 @@ collection组件提供方便的封装来进行数据操作
     $obj = Collection::make(['id'=>1, 'name'=>'willphp']);
     foreach ($obj as $k=>$v) {
 	 	echo $v;
-    }	
-
-####助手函数
-
-    $obj = collect([1, 2, 3]);
+    }
   
--- src/Collection.php
+++ src/Collection.php
@@ -3,14 +3,14 @@
  | Software: [WillPHP framework]
  | Site: www.113344.com
  |--------------------------------------------------------------------------
- | Author: no-mind <24203741@qq.com>
+ | Author: 无念 <24203741@qq.com>
  | WeChat: www113344
  | Copyright (c) 2020-2022, www.113344.com. All Rights Reserved.
  |-------------------------------------------------------------------------*/
 namespace willphp\collection;
-use willphp\collection\build\Base;
+use willphp\page\Page;
 /**
- * 模型多数据集合
+ * 模型数据集合
  * Class Collection
  * @package willphp\collection
  */
@@ -18,11 +18,74 @@ class Collection {
 	protected $link;
 	public function __call( $method, $params ) {
 		if (is_null($this->link ) ) {
-			$this->link = new Base();	
+			$this->link = new CollectionBuilder();	
 		}		
 		return call_user_func_array([$this->link, $method], $params);
 	}
 	public static function __callStatic($name, $arguments) {
 		return call_user_func_array([new static(), $name], $arguments);
+	}
+}
+class CollectionBuilder implements \Iterator, \ArrayAccess {
+	protected $items = [];
+	public function current() {
+		return current($this->items);
+	}
+	public function next() {
+		next($this->items);
+	}
+	public function key() {
+		return key($this->items);
+	}
+	public function valid()	{
+		return current($this->items);
+	}
+	public function rewind() {
+		reset($this->items);
+	}
+	public function offsetExists($offset) {
+		return isset($this->items[$offset]);
+	}
+	public function offsetGet($key)	{
+		return isset($this->items[$key]) ? $this->items[$key] : null;
+	}
+	public function offsetSet($offset, $value) {
+		$this->items[$offset] = $value;
+	}
+	public function offsetUnset($key) {
+		if (isset($this->items[$key])) {
+			unset($this->items[$key]);
+		}
+	}
+	/**
+	 * 转换为数组
+	 * @return array
+	 */
+	public function toArray() {
+		$res = [];
+		foreach ($this->items as $k => $v) {
+			if (is_object($v) && method_exists($v, 'toArray')) {
+				$res[] = $v->toArray();
+			} else {
+				$res[] = $v;
+			}
+		}
+		return $res;
+	}
+	/**
+	 * 设置items值
+	 * @param $data
+	 * @return $this
+	 */
+	public function make($data) {
+		$this->items = $data;
+		return $this;
+	}
+	/**
+	 * 显示分页
+	 * @return mixed
+	 */
+	public function links()	{
+		return Page::single();
 	}
 }
